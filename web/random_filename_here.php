296cb3d53235193471f6362e570f6cc2ed9e0c27
Ayumi Goto <ayumi.goto.0212@gmail.com>
2016-01-15 16:12:54 +0900

    Update random_filename_here.php

--- web/random_filename_here.php
+++ web/random_filename_here.php
@@ -1,4 +1,4 @@
 <?php
-  $res = file_get_contents('https://username@password@example.com/appname/ical/');
+  $res = file_get_contents('https://username:password@example.com/appname/ical/');
   echo $res;
 ?>
